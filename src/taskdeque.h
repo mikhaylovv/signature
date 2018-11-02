56c39bb782584492ca382d8c0d2c94cab3f5eed8
thesanone <thesanone@bk.ru>
2018-11-03 01:45:44 +0300

    build fixed, taskqueu new function + test fixed

--- src/taskdeque.h
+++ src/taskdeque.h
@@ -8,28 +8,36 @@
 #include <deque>
 #include <optional>
 
+/// class provides a method for synchronous operation with the queue
 template<class T>
 class TaskDeque
 {
 public:
-  TaskDeque( std::shared_ptr<std::atomic<bool> > done ) noexcept;
+  TaskDeque() noexcept {};
 
+  TaskDeque( const TaskDeque & other ) = default;
+  TaskDeque( TaskDeque && other ) = default;
+
+  
+  /// Adds an item at the end of the queue and notifies one listener on condition_variable
   void push_back( const std::shared_future<T>& obj );
 
+  /// Returns the feature from the top of the deque, if the deque is empty, waiting for data
+  /// Return empty optional, if done flag seted
   std::optional<std::shared_future<T> > wait_and_take_first();
 
+  /// non-blocking operation
+  /// Sets the done flag when the flag is true take_first always returns an empty optional.
+  /// notify all waiting objects
+  void set_done( bool val );
+
 private:
-  std::atomic<bool> _done;
+  std::shared_ptr<std::atomic<bool> > _done 
+    = std::make_shared<std::atomic<bool> >( false );
   std::mutex _mutex;
   std::condition_variable _cond;
   std::deque<T> _deq;
 };
-
-template<class T>
-TaskDeque<T>::TaskDeque(std::shared_ptr<std::atomic<bool> > done) noexcept
-{
-
-}
 
 template<class T>
 void TaskDeque<T>::push_back( const std::shared_future<T> &obj )
@@ -42,5 +50,12 @@ std::optional<std::shared_future<T> > TaskDeque<T>::wait_and_take_first()
 {
   return std::nullopt;
 }
+
+template<class T>
+void TaskDeque<T>::set_done( bool val )
+{
+  
+}
+
 
 #endif // TASKDEQUE_H
--- tests/worker_test.cpp
+++ tests/worker_test.cpp
@@ -9,30 +9,31 @@ BOOST_AUTO_TEST_SUITE(worker)
 
 BOOST_AUTO_TEST_CASE(worker_results)
 {
-  auto done = std::make_shared<std::atomic<bool> >( false );
-  TaskDeque<int> deq ( done );
+  auto deque = std::make_shared<TaskDeque<int> >();
   std::vector<int> objs = { 10, 20, 30 };
 
-  std::vector<int> result;
-  result.reserve(3);
+  auto result = std::make_shared<std::vector<int> > ();
+  result->reserve(3);
 
-  std::thread thr ( [] ( auto & deq, auto & res ) {
-    while ( auto val = deq.wait_and_take_first() ) {
-      BOOST_CHECK_NO_THROW( res.push_back( ( *val ).get() ) );
+  std::thread thr ( [] ( auto deq, auto res ) {
+    while ( auto val = deq->wait_and_take_first() ) {
+      BOOST_CHECK_NO_THROW( res->push_back( ( *val ).get() ) );
     }
-  }, deq, result );
+  }, deque, result );
 
   for ( auto obj : objs ) {
-    deq.push_back(
+    deque->push_back(
           std::async(
             [] (auto ob) { return ob / 10; }
             , obj ).share() );
   }
+  deque->set_done( true );
 
   thr.join();
 
+  BOOST_REQUIRE( result->size() == 3 );
   for ( size_t i = 0; i < 3; ++i ) {
-    BOOST_CHECK( objs[i] / 10 == result[i] );
+    BOOST_CHECK( objs.at( i ) / 10 == result->at( i ) );
   }
 }
 
